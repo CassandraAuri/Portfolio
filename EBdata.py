5741348159fd8cb80c844ffec0487bda984d928f
CassandraAuri <105663287+CassandraAuri@users.noreply.github.com>
2023-02-16 21:24:38 -0700

    Update EBdata.py

--- EBdata.py
+++ EBdata.py
@@ -5,25 +5,32 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import datetime as dt
 from tqdm.notebook import tqdm
+import mplcyberpunk
 set_token("https://vires.services/ows", set_default=True,
           token="kmxv5mTTyYwzw4kQ9lsCkGfQHtjjJRVZ")  # key
+
+plt.style.use("cyberpunk")  # Dark mode!
+
 fig, axes = plt.subplots(nrows=3,
-                         figsize=(10, 6),
+                         figsize=(10, 6), sharex=True, sharey=False
                          )
-plt.style.use('dark_background')
+
 start_time = dt.datetime(2021, 3, 18, 8, 13)
 end_time = dt.datetime(2021, 3, 18, 8, 22)  # start and end time
+
 labels = ["Swarm A", "Swarm B", "Swarm C"]
 measurements = ["B_NEC", "Ehx"]
+
+
 collectionE = [
     "SW_EXPT_EFIA_TCT02",
     "SW_EXPT_EFIB_TCT02",
     "SW_EXPT_EFIC_TCT02",
 ]
 collectionB = [
-    "SW_OPER_MAGA_HR_1B",
-    "SW_OPER_MAGB_HR_1B",
-    "SW_OPER_MAGC_HR_1B"
+    ["SW_OPER_MAGA_LR_1B", "SW_OPER_MAGA_HR_1B"],
+    ["SW_OPER_MAGB_LR_1B", "SW_OPER_MAGB_HR_1B"],
+    ["SW_OPER_MAGC_LR_1B", "SW_OPER_MAGC_HR_1B"]
 ]
 collectionF = [
     "SW_OPER_FACATMS_2F",
@@ -35,7 +42,8 @@ def requester(sc_collection, measurement, **kwargs):
     try:
         request = SwarmRequest()
         request.set_collection(sc_collection)
-        request.set_products(measurements=measurement)
+        request.set_products(measurements=measurement, models="CHAOS")
+
         data = request.get_between(start_time, end_time, **kwargs)
         df = data.as_dataframe()
     except:
@@ -44,52 +52,73 @@ def requester(sc_collection, measurement, **kwargs):
 
 
 def graphingB(index, arrayx, arrayy):
-    def Delta():
-        delta_yarray = np.zeros(len(arrayy))
-        baseline = np.linspace(arrayy[0], arrayy[-1], len(arrayy))
-        for i in range(len(arrayy)):
-            delta_yarray[i] = arrayy[i]-baseline[i]
-        return delta_yarray
-    delta_arrayy = Delta()  # assumes a linear slope, gets rid of baseline
-    color = ['dodgerblue', 'purple', 'deeppink', "orange"]
-    axes[0].plot(arrayx, delta_arrayy,
+    color = ['gold', 'cyan', 'deeppink', "red"]
+    axes[0].plot(arrayx, arrayy,
                  color=color[index], label="B_E, "+labels[index])
     axes[0].legend()
+    axes[0].set_ylabel(r"$\Delta$  $B_{E}$ (nT)")
 
 
 def graphingF(index, arrayx, arrayy):
-    color = ['dodgerblue', 'purple', 'deeppink', "orange"]
+    color = ['gold', 'cyan', 'deeppink', "red"]
     axes[2].plot(arrayx, arrayy,
                  color=color[index], label="FAC, "+labels[index])
     axes[2].legend()
+    axes[2].set_ylabel("FAI Intensity (kR)")
 
 
 def graphingE(index, dataset, arrayx, arrayy):
-    color = ['dodgerblue', 'purple', 'deeppink', "orange"]
-    if(dataset == "Ehx"):
-        axes[1].plot(arrayx, arrayy,
-                     color=color[index+2], label=dataset+","+labels[index])
-    else:
-        axes[1].plot(arrayx, arrayy,
-                     color=color[index], label=dataset+","+labels[index])
+    color = ['gold', 'cyan', 'deeppink', "red"]
+    axes[1].plot(arrayx, arrayy,
+                 color=color[index], label=dataset+","+labels[index])
+    axes[1].set_ylabel(r"$E_{N}$ (mV/m)$")
     axes[1].legend()
 
 
 def requesterarraylogic():
     def B():
+        def High_resolution_interpolation(time_model, b_model, proper_time):
+            # creates an empty array of proper len, which has less resolution
+            Bmodel_attime = np.zeros(len(proper_time))
+            for i in range(len(proper_time)):
+                Bmodel_attime[i] = (b_model[np.argmin(  # Optimization, finds the minimum value between the subtraction of the times, then finds the corresponding model B field
+                    np.abs(proper_time[i]-time_model))])
+            return Bmodel_attime
+
         for i in range(len(collectionB)):
+            bmodel = []
             b = []
-            ds = requester(
-                collectionB[i], measurements[0], asynchronous=False, show_progress=False)
-            Ball = ds["B_NEC"]
-            time = Ball.index
-            Ball = pd.Series.to_numpy(Ball)
+            time = []
+            for k in range(2):
+                ds = requester(
+                    collectionB[i][k], measurements[0], asynchronous=False, show_progress=False)  # Data package level, data/modes, **kwargs
+                if(k == 0):
+                    Ball = ds["B_NEC"]  # data package
+                    # Finds the time which is stored as a row header (ie row name)
+                    time = Ball.index
+                    #time = np.delete(time, -1)
+                    Ball = pd.Series.to_numpy(Ball)  # turns to numpy array
+                    # for i in range(len(Ball))
+                    for j in range(len(Ball[:])):
+                        # flattens array for the Northward data ie 2nd index
+                        b.append(Ball[:][j][1])
+                else:
+                    pass
+                if(k == 1):
+                    Bmodel = ds["B_NEC_CHAOS"]
+                    # print(Bmodel)
+                    time_model = Bmodel.index
+                    Bmodel = pd.Series.to_numpy(Bmodel)  # turns to numpy array
+                    # for i in range(len(Ball))
+                    for j in range(len(Bmodel[:])):
+                        # flattens array for the Northward data ie 2nd index
+                        bmodel.append(Bmodel[:][j][1])
+                    Model = High_resolution_interpolation(
+                        time_model, bmodel, time)
 
-            # for i in range(len(Ball))
-            for j in range(len(Ball[:])):
-                b.append(Ball[:][j][1])
-
-            graphingB(i, time, b)
+                else:
+                    pass
+            graphingB(i, time, b-Model)
 
     def E():
         lens = len(collectionE)*2
@@ -123,5 +152,11 @@ def requesterarraylogic():
 
 requesterarraylogic()
 
-plt.style.use('dark_background')
+fig.supxlabel("Time (Day:Hour:Minute)")
+fig.suptitle("Time Versus Auroral Parameters From Swarm Spacecraft")
+
+for i in range(len(axes)):  # final touches
+    mplcyberpunk.make_lines_glow(axes[i])  # adds glow to plots
+    # mplcyberpunk.add_gradient_fill(
+    # ax=axes[i], alpha_gradientglow=0.8, gradient_start="zero")
 plt.show()
