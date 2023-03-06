b704d50ee17e533c8b53fbf479df3fee1b90093f
CassandraAuri <105663287+CassandraAuri@users.noreply.github.com>
2023-03-05 22:08:54 -0700

    Update MFA.py

--- MFA.py
+++ MFA.py
@@ -5,11 +5,15 @@ def MFA(X, meanX, U):  # Transforms X, meanX, and the position vector of the sal
     Xmfa = np.zeros((3, len(X)))
     bmean = np.sqrt(meanX[0]**2+meanX[1]**2+meanX[2]**2)
     xmfa_unitary2 = bmean/meanX
+    print(np.shape(X))
     Xmfa[2] = np.dot(X, np.array(xmfa_unitary2).T)
+
     Xazim = np.cross(np.array(xmfa_unitary2), U)
+    Xazim = np.average(Xazim, axis=0)
     bazim = np.sqrt(Xazim[0]**2+Xazim[1]**2+Xazim[2]**2)
     xmfa_unitary1 = Xazim/bazim
     Xmfa[1] = np.dot(X, np.array(xmfa_unitary1).T)
+
     xpol = np.cross(xmfa_unitary1, xmfa_unitary2)
     bxpol = np.sqrt(xpol[0]**2+xpol[0]**2+xpol[0]**2)
     xmfa_unitary0 = xpol/bxpol
