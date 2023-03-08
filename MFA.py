23bb743aa621c2e6fd1a821834cfc3fda195643a
CassandraAuri <105663287+CassandraAuri@users.noreply.github.com>
2023-03-08 09:54:16 -0700

    Update MFA.py

--- MFA.py
+++ MFA.py
@@ -1,21 +1,55 @@
 import numpy as np
 
 
-def MFA(X, meanX, U):  # Transforms X, meanX, and the position vector of the sallite into vectors MFA and transforms V into MFA
-    Xmfa = np.zeros((3, len(X)))
-    bmean = np.sqrt(meanX[0]**2+meanX[1]**2+meanX[2]**2)
-    xmfa_unitary2 = bmean/meanX
-    print(np.shape(X))
-    Xmfa[2] = np.dot(X, np.array(xmfa_unitary2).T)
 
-    Xazim = np.cross(np.array(xmfa_unitary2), U)
-    Xazim = np.average(Xazim, axis=0)
-    bazim = np.sqrt(Xazim[0]**2+Xazim[1]**2+Xazim[2]**2)
-    xmfa_unitary1 = Xazim/bazim
-    Xmfa[1] = np.dot(X, np.array(xmfa_unitary1).T)
+def MFA(B_NEC, B_MEAN_NEC, R_NEC):
+    def magn(X):
+        """
+        Return the row-wise magnitude of elements in 2D array 'X' as a single-column array.
+        """
+        return np.reshape(np.sqrt(np.sum(X**2, axis=1)), (-1, 1))
+    """ """
 
-    xpol = np.cross(xmfa_unitary1, xmfa_unitary2)
-    bxpol = np.sqrt(xpol[0]**2+xpol[0]**2+xpol[0]**2)
-    xmfa_unitary0 = xpol/bxpol
-    Xmfa[0] = np.dot(X, np.array(xmfa_unitary0).T)
-    return Xmfa
+    # if no positional vector is given just assume the direction (0,0,-1) in NEC
+    # coordinates, i.e. radial outwards (the magnitude is not necessary, only
+    # the direction matters in order to compute its cross product with the mean
+    # field component
+    if R_NEC is None:
+        R_NEC = np.zeros(B_NEC.shape)
+        R_NEC[:, 2] = -1
+    #print(R_NEC)
+    MFA = np.full(B_NEC.shape, np.NaN)
+    # create the unitary vector of the mean field
+    B_MEAN = magn(B_MEAN_NEC)
+    B_MEAN_UNIT = B_MEAN_NEC / B_MEAN
+    # find the field along the mean field direction
+    MFA[:, 2] = np.sum(B_NEC * B_MEAN_UNIT, axis=1)
+
+    # find the direction of the azimuthal component
+    B_AZIM = np.cross(B_MEAN_UNIT, R_NEC)
+    B_AZIM_UNIT = B_AZIM / magn(B_AZIM)
+    # find the field along the azimuthal direction
+    MFA[:, 1] = np.sum(B_NEC * B_AZIM_UNIT, axis=1)
+
+    # find the direction of the poloidal component
+    B_POL = np.cross(B_AZIM_UNIT, B_MEAN_UNIT)
+    B_POL_UNIT= B_POL / magn(B_POL)
+    # no need to 
+    MFA[:, 0] = np.sum(B_NEC * B_POL_UNIT, axis=1)
+    # test that magnitude is conserved
+    return MFA
+
+
+B=np.array([
+            [1,2,3],
+            [2,3,4,]
+            ])
+MeanX=np.array(
+    [[1.1,2.1,3.1],
+     [3,5,4]
+     ])
+R=None
+print(np.shape(B))
+mfa=MFA(B,MeanX,R)
+#print(mfa)
+#print(mfa[:,0])
